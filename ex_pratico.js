f6d7693a92abe42f77e4c3715e8be6aab89938c4
Victor Hugo Favaro Moreira <118476125+zVihugo@users.noreply.github.com>
2024-03-10 21:17:55 -0300

    Add files via upload

---
+++ ex_pratico.js
@@ -0,0 +1,7 @@
+//implemente uma função que ao clicar no botão exercício 1 a seguir, insira o valor do input como um elemento li no elemento ul a seguir. Utilize o conceito de string interpolation
+
+
+// <input type="text" id="input1">
+// <button id="btn1">Exercício 1</button>
+// <ul id="ul1"></ul>
+
